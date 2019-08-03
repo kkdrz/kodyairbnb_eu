e35c4180673ae72971380c448fdd154d5f6b18be
Konrad Drozd <drozdkonrad@gmail.com>
2019-08-03 14:53:02 +0200

    remove useless starter parts

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+prod_site_dir="/var/www/kodyairbnb_eu"
+dev_site_dir="/home/konrad/projects/kodyairbnb/public"
+ssh_connection="root@212.237.11.153"
+
+ssh $ssh_connection "sudo rm -rf $prod_site_dir && sudo mkdir $prod_site_dir"
+
+
+echo "Cleaned directory: $prod_site_dir"
+
+(
+	cd $dev_site_dir;
+	scp -r * "$ssh_connection:$prod_site_dir"
+)
+
+
+echo "Copied $dev_site_dir to $ssh_connection:$prod_site_dir"
+
+ssh $ssh_connection "sudo systemctl restart nginx.service"
+
+echo "Restarted NGINX"
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
@@ -32,8 +25,8 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      title={title || site.siteMetadata.title}
+      titleTemplate={`%s`}
       meta={[
         {
           name: `description`,
@@ -51,29 +44,13 @@ function SEO({ description, lang, meta, title }) {
           property: `og:type`,
           content: `website`,
         },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
       ].concat(meta)}
     />
   )
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: `pl`,
   meta: [],
   description: ``,
 }
