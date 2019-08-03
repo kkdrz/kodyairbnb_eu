b0d868daacd931f2d7e055ee906fa090f389a0ed
Konrad Drozd <drozdkonrad@gmail.com>
2019-08-03 22:33:16 +0200

    Fix build error

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 import styles from "./container.module.scss"
-require("smooth-scroll")('a[href*="#"]')
+if (typeof window !== "undefined") {
+  require("smooth-scroll")('a[href*="#"]')
+}
 
 const Container = ({ children }) => (
-  
-    <div className={styles.container}>{children}</div>
-  
+  <div className={styles.container}>{children}</div>
 )
 
 export default Container
